27b8cb4a80ee72832f04ad1802d7f7b45aefdc13
Tom Oram <tom@codept.com>
2016-12-04 22:28:26 +0000

    Remove example tests

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # elm-siren
-An Elm parser for Siren APIs
+An Elm parser for Siren APIs.
+
+## TODO
+Remaining items for first release:
+
+- [ ] Add embedded entity representations
+- [ ] Add actions to entities
+- [ ] Add action fields
+- [ ] Add embedded entities
+- [ ] Add list entity properties
+- [ ] Add object entity properties
--- tests/Main.elm
+++ tests/Main.elm
@@ -1,7 +1,6 @@
 port module Main exposing (..)
 
 import Test exposing (describe)
-import Tests
 import SirenTests
 import Test.Runner.Node exposing (run, TestProgram)
 import Json.Encode exposing (Value)
@@ -11,8 +10,7 @@ main : TestProgram
 main =
     run emit
         (describe "Elm Siren"
-            [ Tests.all
-            , SirenTests.all
+            [ SirenTests.all
             ]
         )
 
